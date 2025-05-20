1820a15cef502911f70e488d1af0d6574c7ed337
DeDxYk594 <zhugeo@mail.ru>
2025-05-20 23:06:27 +0300

    refactor plastinka-sapr: organize files into directories

---
+++ include/curves/arc.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <algorithm>
+
+#include "base.hpp"
+#include "curve.hpp"
+
+namespace plastinka_sapr::curves
+{
+
+    class Arc : public Curve
+    {
+    public:
+        /** Угол равен нулю при направлении (0, 1); дальше по часовой стрелке */
+        Arc(Point center, double R, double startAngle = 0.0, double stopAngle = 360.0) : center(center),
+                                                                                         R(R),
+                                                                                         startAngle(std::min(startAngle, stopAngle)),
+                                                                                         stopAngle(std::max(startAngle, stopAngle)) {}
+        virtual std::vector<double> xIntersections(double yLevel) const override;
+        virtual std::vector<double> yIntersections(double xLevel) const override;
+
+        virtual std::pair<double, double> xBoundaries() const override;
+        virtual std::pair<double, double> yBoundaries() const override;
+        virtual std::pair<double, double> getNormal(Point p) const override;
+
+    private:
+        Point center;
+        double R;
+        double startAngle, stopAngle;
+    };
+
+} // namespace plastinka_sapr::curves
---
+++ include/curves/curve.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "base.hpp"
+
+namespace plastinka_sapr::curves
+{
+
+    class Curve
+    {
+    public:
+        virtual std::vector<double> xIntersections(double yLevel) const = 0;
+        virtual std::vector<double> yIntersections(double xLevel) const = 0;
+        virtual std::pair<double, double> xBoundaries() const = 0;
+        virtual std::pair<double, double> yBoundaries() const = 0;
+        virtual std::pair<double, double> getNormal(Point p) const = 0;
+        virtual ~Curve() {};
+    };
+
+    typedef std::shared_ptr<Curve> CurvePtr;
+
+} // namespace plastinka_sapr::curves
